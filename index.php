354059c505ac2670061408606ad45ca99ef30dec
Hissok <fm64@outlook.fr>
2021-03-20 14:42:32 +0100

    Ajout de la fonctionnalité custom validator

--- index.php
+++ index.php
@@ -17,19 +17,12 @@
     $form->add(BrickForm\Field::class);
     $form->add(BrickForm\PasswordField::class, ['toconfirm']);
 
-    $form->getElementByName('username')->addCustomValidator('test1');
-    $form->getElementByName('username')->addCustomValidator('test2');
-
     echo $form->getView();
 
     ?>
 
 </body>
 
-<script>
-    var brickform_json_data = <?php echo BrickForm\Form::getCustomValidatorsAsJSON(); ?>
-</script>
-<script type="text/javascript" src="testfunc.js" defer></script>
 <script type="text/javascript" src="brickform-validations.js" defer>
 
 </script>
--- testfunc.js
+++
@@ -1,7 +0,0 @@
-var test1 = function (field) {
-    return [0, 'Une erreur personnalisée'];
-}
-
-var test2 = function (field) {
-    return [0, 'Une deuxième erreur personnalisée'];
-}
